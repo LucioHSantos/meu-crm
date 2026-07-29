401b2ee34f6b5b6e8494296b78ef290de7a83cc6
LucioHSantos <luhen2005@hotmail.com>
2026-07-29 17:56:09 -0300

    fix: add localhost to ALLOWED_HOSTS for bridge communication

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='yourdomain.com').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='yourdomain.com').split(',') + ['localhost', '127.0.0.1']
 
 DATABASES = {
     'default': {
